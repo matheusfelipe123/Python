ebd349d06ba65db15bcdbf17408179f9b3f1663f
matheusfelipe123 <35380991+matheusfelipe123@users.noreply.github.com>
2024-03-27 11:59:30 -0300

    Update and rename Exercicio1.PY to Ex1.py

---
+++ execicios/Primeiro_Trimestre/Lista_Prova/Ex1.py
@@ -0,0 +1,12 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+def soma(a,b):
+    return(a+b)
+
+
+a = int(input("Digite um valor"))
+b = int(input("Digite um segundo valor"))
+
+print(f'soma of {a} and {b} is {soma(a, b)}')
+
+
