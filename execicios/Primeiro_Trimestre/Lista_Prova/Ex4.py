e649d65bf2a68cd2587e30cbd5f7d681d9f63794
matheusfelipe123 <35380991+matheusfelipe123@users.noreply.github.com>
2024-03-27 12:29:18 -0300

    Update Ex4.py

--- execicios/Primeiro_Trimestre/Lista_Prova/Ex4.py
+++ execicios/Primeiro_Trimestre/Lista_Prova/Ex4.py
@@ -1,1 +1,9 @@
+nota1 = int(input("Digite nota1"))
+nota2 = int(input("Digite nota2"))
+nota3 = int(input("Digite nota3"))
+nota4 = int(input("Digite nota4"))
+
+media=(nota1+nota2+nota3+nota4)/4
+
+print(media)
 
