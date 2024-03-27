e34fd1e4bcb629b0c053c34f438f92afd5e07dca
matheusfelipe123 <35380991+matheusfelipe123@users.noreply.github.com>
2024-03-27 12:13:51 -0300

    Update Ex2.py

--- execicios/Primeiro_Trimestre/Lista_Prova/Ex2.py
+++ execicios/Primeiro_Trimestre/Lista_Prova/Ex2.py
@@ -1,1 +1,12 @@
+numero = int(input("Digite um valor menor que 100"))
 
+if numero >=100:
+  print("o numero inserido não e menor que 100")
+else:
+    
+  dezena = numero //10 
+  unidade = numero % 10
+  
+  soma_digitos = dezena+unidade
+  print(soma_digitos)
+  
