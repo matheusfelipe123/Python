13dd316fdcaa9ac3d5c2190607f6dc8ab689c4e6
matheusfelipe123 <35380991+matheusfelipe123@users.noreply.github.com>
2024-04-05 11:35:17 -0300

    Update Ex6.py

--- execicios/Primeiro_Trimestre/Lista_Prova/Ex6.py
+++ execicios/Primeiro_Trimestre/Lista_Prova/Ex6.py
@@ -1,1 +1,8 @@
+def sum(a, b, c):
+    return (a + b + c)
 
+a = int(input('Digite seu primeiro Numero: '))
+b = int(input('Digite seu segundo Numero: '))
+c = int(input('Digite seu Terceiro Numero: '))
+
+print(a + b + c)
