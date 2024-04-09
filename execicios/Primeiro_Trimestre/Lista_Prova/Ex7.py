be9e809e954230aa2e4e4d9438aab313f45554e9
matheusfelipe123 <35380991+matheusfelipe123@users.noreply.github.com>
2024-04-08 22:03:14 -0300

    Update Ex7.py

--- execicios/Primeiro_Trimestre/Lista_Prova/Ex7.py
+++ execicios/Primeiro_Trimestre/Lista_Prova/Ex7.py
@@ -2,12 +2,11 @@ numero1 = int(input('Digite seu primeiro Numero: '))
 numero2 = int(input('Digite seu segundo Numero: '))
 numero3 = int(input('Digite seu Terceiro Numero: '))
 
-if numero1 => numero2 or numero3
-   print("Esse e o numero maior")
-elif numero2 => numero1 or numero3
-    print("Esse e o numero maior")
-else numero3 
-    print ("Esse e o numero maior")
+if (numero1 >= numero2) and (numero1 >= numero3):
+   maior = numero1
+elif (numero2 >= numero1) and (numero2 >= numero3):
+    maior = numero2
+else:
+    maior = numero3
 
-print(f'Sum of {numero1} and {numero2} and {numero3} is {(numero1, numero2, numero3)}')
-
+print("O maior numero é:",maior)
