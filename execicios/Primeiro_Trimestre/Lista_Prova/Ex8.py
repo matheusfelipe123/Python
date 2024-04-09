07c18455d88aaa68007ee97756a56d279dc2a036
matheusfelipe123 <35380991+matheusfelipe123@users.noreply.github.com>
2024-04-08 22:10:23 -0300

    Update Ex8.py

--- execicios/Primeiro_Trimestre/Lista_Prova/Ex8.py
+++ execicios/Primeiro_Trimestre/Lista_Prova/Ex8.py
@@ -1,1 +1,13 @@
+lado1 = float(input("Insira o comprimento do primeiro lado:"))
+lado2 = float(input("Insira o comprimento do segundo lado:"))
+lado3 = float(input("Insira o comprimento do terceiro lado:"))
 
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    if lado1 == lado2 == lado3:
+        print("O triângulo é equilátero.")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print("O triângulo é isósceles.")
+    else:
+        print("O triângulo é escaleno.")
+else:
+    print("Os comprimentos dos lados dados não podem formar um triângulo.")
